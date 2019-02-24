94b7591df77935f95f15424785f59e7866665ecf
Alex Prokoshev <3232prokoshev@mail.ru>
2019-02-24 13:41:15 +0300

    ждобавил файл

---
+++ round.py
@@ -0,0 +1,27 @@
+import random
+import antigravity
+round(3.67, 1)
+print(round(3.6789778,5))
+print(2!=3)
+print()
+ljlk
+
+"""def answer(num):
+    if num == 1:
+        return 'one'
+    elif num == 2:
+        return "two"
+    elif num == 3:
+        return 'three'
+print(answer(random.randint(1,3)))"""
+
+def spam(div):
+ try:
+    return 10/div
+ except ZeroDivisionError:
+     print('ошибка деления на ноль')
+
+print(spam(0))
+print(spam(2))
+print(spam(3))
+
