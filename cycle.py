47fb00a34dc38fc070c4d9b9abdb53e0628cbbed
Alex Prokoshev <3232prokoshev@mail.ru>
2019-07-07 10:12:08 +0300

    Добавил три файла эксель,цикл,интернет

---
+++ cycle.py
@@ -0,0 +1,10 @@
+for x in range(0,10):
+    print(x)
+    if x==5:
+        break
+
+x=1
+while True:
+    print(x)
+    x=x+1
+    
---
+++ excel.py
@@ -0,0 +1,4 @@
+import openpyxl
+wb=openpyxl.load_workbook('книга1.xlsx')
+type(wb)
+wb.get_sheet_names()
